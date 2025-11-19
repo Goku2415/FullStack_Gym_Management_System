ed84e5630f1d420e59d5efaa56073f5b0ac099f6
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-20 00:58:24 +0530

    first fullstack project

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Login from '../../Components/Login/login.jsx'
+import SignUp from '../../Components/Signup/signUp.jsx'
+
+const Home = () => {
+  return (
+    <div className='w-full h-[100vh]'>
+        <div className='border-2 border-slate-800 bg-slate-800 text-white p-5 font-semibold text-xl'>
+            Welcome To Gym Management System 
+        </div>
+        <div className='w-full bg-cover flex justify-center h-[100%] bg-[url("./1.jpg")]'>
+           <div className="w-full flex gap-32 ">
+
+            <Login/>
+
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
