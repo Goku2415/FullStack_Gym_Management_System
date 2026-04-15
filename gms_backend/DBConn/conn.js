4e318808c4a25f147af216ca0671ecf2fc1446e1
unknown <maheshsinghgaria24@gmail.com>
2026-04-16 05:26:11 +0530

    changes for render

--- gms_backend/DBConn/conn.js
+++ gms_backend/DBConn/conn.js
@@ -4,6 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connection successful");
+    console.log("Connecting to DB...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
   } catch (err) {
     console.error("DB connection failed:", err.message);
   }
--- gym-management-frontend/src/Components/Login/login.jsx
+++ gym-management-frontend/src/Components/Login/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-         "https://your-backend.vercel.app/api/auth/login",
+         "https://gym-backend-qrp7.onrender.com",
         loginField,
         { withCredentials: true },
       );
