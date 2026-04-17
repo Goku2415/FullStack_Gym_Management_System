53b43a9aa77d15d5725d2311a0ec455aec7be7b1
unknown <maheshsinghgaria24@gmail.com>
2026-04-17 16:07:38 +0530

    the one last change

--- gms_backend/Auth/auth.js
+++ gms_backend/Auth/auth.js
@@ -1,34 +1,30 @@
 const Gym = require("../Modals/gym");
 const jwt = require("jsonwebtoken");
 
-
 const auth = async (req, res, next) => {
     try {
-        const token = req.cookies.cookieToken;
-        
-        if (!token) {
-            return res.status(401).json({ error: "No token found" });
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({ error: "No token provided" });
         }
 
+        const token = authHeader.split(" ")[1]; // Bearer token
+
         const decode = jwt.verify(token, process.env.JWT_SecretKey);
-        console.log("Decoded:", decode);
 
         const gym = await Gym.findById(decode.gym_id);
-        console.log("Gym found:", gym);
 
         if (!gym) {
-            return res.status(401).json({ error: "Gym not found in DB" });
+            return res.status(401).json({ error: "Gym not found" });
         }
 
         req.gym = gym;
         next();
 
     } catch (err) {
-        console.log("AUTH ERROR:", err.message);
         return res.status(401).json({ error: err.message });
     }
 };
-
-
 
 module.exports = auth;
